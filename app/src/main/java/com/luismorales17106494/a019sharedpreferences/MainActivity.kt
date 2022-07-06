2bfd4a188d4fac331e6a7e796527dcd7c2921046
Edu-Lu <moralesluis17106494@gmail.com>
2022-07-06 13:34:23 -0500

    tutorial sin hacer

---
+++ app/src/main/java/com/luismorales17106494/a019sharedpreferences/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.luismorales17106494.a019sharedpreferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+
+Como usar SharedPreferences Curso de Android AristiDevs
+019 Video
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
